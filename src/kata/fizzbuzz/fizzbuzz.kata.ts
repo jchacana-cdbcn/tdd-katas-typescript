355bd0a90499fda222fa15c40b1563a85407e924
Javier Chacana <javier.chacana@codurance.com>
2020-10-19 22:06:47 +0200

    Green: Second case + refactor for most most obvious implementation
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/fizzbuzz/fizzbuzz.kata.ts
+++ src/kata/fizzbuzz/fizzbuzz.kata.ts
@@ -1,3 +1,3 @@
 export const fizzbuzz = (num: number): string  => {
-    return "1";
+    return num === 1? "1" : "2";
 }
--- src/kata/fizzbuzz/fizzbuzz.spec.ts
+++ src/kata/fizzbuzz/fizzbuzz.spec.ts
@@ -3,4 +3,7 @@ describe('fizzbuzz kata', () => {
     it("should return 1 when number 1", () => {
         expect(fizzbuzz(1)).toBe("1")
     })
+    it("should return 2 when number 2", () => {
+        expect(fizzbuzz(2)).toBe("2")
+    })
 })
