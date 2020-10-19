b35ee1b24fb7cc91edd06e8cbe7961510b7c2e28
Javier Chacana <javier.chacana@codurance.com>
2020-10-19 22:28:59 +0200

    Refactor: generalization for cases where the number is multiple of 5
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/fizzbuzz/fizzbuzz.kata.ts
+++ src/kata/fizzbuzz/fizzbuzz.kata.ts
@@ -1,9 +1,13 @@
 export const fizzbuzz = (num: number): string  => {
     if(isMultipleOf3(num)) return "Fizz" 
-    if(num === 5 || num === 10 || num === 20) return "Buzz"
+    if(isMultipleOf5(num)) return "Buzz"
     return num.toString();
 }
 
 const isMultipleOf3 = (num: number): boolean => {
     return num % 3 === 0;
 }
+const isMultipleOf5 = (num: number): boolean => {
+    return num % 5 === 0;
+}
+
