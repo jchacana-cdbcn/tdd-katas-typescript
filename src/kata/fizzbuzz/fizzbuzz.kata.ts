6ba91bea2d62ea14bfa1c9663c87bff6abeddca3
Javier Chacana <javier.chacana@codurance.com>
2020-10-19 22:18:31 +0200

    Green: Most obvious implementation for case 6
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/fizzbuzz/fizzbuzz.kata.ts
+++ src/kata/fizzbuzz/fizzbuzz.kata.ts
@@ -1,5 +1,5 @@
 export const fizzbuzz = (num: number): string  => {
-    if(num === 3) return "Fizz"
+    if(num === 3 || num === 6) return "Fizz"
     if(num === 5) return "Buzz"
     return num.toString();
 }
