f08be7b01d3998ab8716ccfc8a4132f01f2aaa9d
Javier Chacana <javier.chacana@codurance.com>
2020-10-22 23:46:52 +0200

    Oversimplification of cases 4, 9, and 40
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman/roman.ts
+++ src/kata/roman/roman.ts
@@ -5,33 +5,30 @@ export class Roman {
         let roman = "";
         let nearest: number = this.getNearestNumber(decimal);
         if(decimal >= nearest) {
-            const nearestUnit = this.getNearestUnit(decimal);
-            const newNearest = this.getNearestNumber(decimal + nearestUnit);
-            if(newNearest - nearestUnit === decimal) {
-                return this.getNearestSymbol(nearestUnit) + this.convert(newNearest);
-            }
             decimal -= nearest;
             return this.getNearestSymbol(nearest) + this.convert(decimal);
         }
         return roman;
     }
 
-    private getNearestNumber(num: number): number {
+    getNearestNumber = (num: number): number => {
         if(num >= 50) return 50;
+        if(num >= 40) return 40
         if(num >= 10) return 10;
+        if(num >= 9) return 9;
         if(num >= 5) return 5;
+        if(num >= 4) return 4;
         return 1;
     }
 
-    private getNearestSymbol(num: number): string {
+    getNearestSymbol = (num: number): string => {
         if(num === 50) return "L";
+        if(num === 40) return "XL";
         if(num === 10) return "X";
+        if(num === 9) return "IX";
         if(num === 5) return "V";
+        if(num === 4) return "IV";
         return "I";
     }
 
-    private getNearestUnit(num: number): number {
-        if(num % 10 === 0) return 10;
-        return 1;
-    }
 }
