9fcbb159499a659fdcc50d51f4cbacbd71ed1bb7
Javier Chacana <javier.chacana@codurance.com>
2020-10-22 20:28:51 +0200

    Green with most obvious implementation for case 7
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman/roman.ts
+++ src/kata/roman/roman.ts
@@ -3,6 +3,7 @@ export class Roman {
     convert = (num: number): string => {
         if(num === 5) return "V";
         if(num === 6) return "VI";
+        if(num === 7) return "VII";
         let result = "";
         for(let i = 0; i < num; i++) {
             result += "I";
