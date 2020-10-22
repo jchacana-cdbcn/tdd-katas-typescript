ac0cb1632255002239ce0167f40001ffabbb2df7
Javier Chacana <javier.chacana@codurance.com>
2020-10-22 22:34:42 +0200

    Refactor to recursive algorithm
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman/roman.ts
+++ src/kata/roman/roman.ts
@@ -3,19 +3,24 @@ export class Roman {
     
     convert = (num: number): string => {
         let result = "";
-        while(num >= 10) {
-            result += "X";
-            num -= 10;
+        let nearest: number = this.getNearestNumber(num);
+        if(num >= nearest) {
+            num -= nearest;
+            return this.getNearestSymbol(nearest) + this.convert(num);
         }
-        if(num === 4 || num === 9) return "I" + this.convert(num + 1);
-        if(num >= 5) {
-            result = "V";
-            num -= 5;
-        }
-        while(num >= 1) {
-            result += "I";
-            num -= 1;
-        }
+        //if(num === 4 || num === 9) return "I" + this.convert(num + 1);
         return result;
+    }
+
+    private getNearestNumber(num: number) {
+        if(num >= 10) return 10;
+        if(num >= 5) return 5;
+        return 1;
+    }
+
+    private getNearestSymbol(num: number) {
+        if(num === 10) return "X";
+        if(num === 5) return "V";
+        return "I";
     }
 }
