a8c68b826468157f4e67212b9b9c47ef9ca90316
Javier Chacana <javier.chacana@codurance.com>
2020-10-22 22:16:13 +0200

    Refactor: simplest algoritthm for now
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman/roman.ts
+++ src/kata/roman/roman.ts
@@ -1,28 +1,19 @@
-interface MyEnum {
-    [key: number]: string
-}
-
-var romanToDecimal: MyEnum = {
-    1: "I",
-    5: "V",
-    10: "X"
-}
 
 export class Roman {
     
     convert = (num: number): string => {
         let result = "";
         while(num >= 10) {
-            result += romanToDecimal[10];
+            result += "X";
             num -= 10;
         }
-        if(num === 4 || num === 9) return romanToDecimal[1] + this.convert(num + 1);
+        if(num === 4 || num === 9) return "I" + this.convert(num + 1);
         if(num >= 5) {
-            result = romanToDecimal[5];
+            result = "V";
             num -= 5;
         }
         while(num >= 1) {
-            result += romanToDecimal[1];
+            result += "I";
             num -= 1;
         }
         return result;
