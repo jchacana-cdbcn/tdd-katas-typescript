8f86d4cad6533f299673cc923bdef9079edcc37b
Javier Chacana <javier.chacana@codurance.com>
2020-10-22 20:45:29 +0200

    Green: started transformation for algorithm
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman/roman.ts
+++ src/kata/roman/roman.ts
@@ -1,15 +1,19 @@
 export class Roman {
     
+    romanOne: string = "I";
+    romanFive: string = "V";
+    romanTen: string = "X";
+
     convert = (num: number): string => {
-        if(num === 10) return "X";
-        if(num === 4) return "IV";
+        if(num === 10) return this.romanTen;
+        if(num === 4) return this.romanOne + this.romanFive;
         let result = "";
         if(num >= 5) {
-            result = "V";
+            result = this.romanFive;
             num -= 5;
         }
         for(let i = 0; i < num; i++) {
-            result += "I";
+            result += this.romanOne;
         }
         return result;
     }
