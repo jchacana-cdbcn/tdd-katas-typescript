e75db2c52d9be6d2b797962308ee41bf8977145d
Javier Chacana <javier.chacana@codurance.com>
2020-10-22 20:18:10 +0200

    Added starter for roman TPP
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

---
+++ src/kata/roman/roman.spec.ts
@@ -0,0 +1,15 @@
+import { Roman } from "./roman";
+
+describe("Roman converter ", () => {
+    const roman: Roman = new Roman();
+    it("should return I for 1", () => {
+        expect(roman.convert(1)).toBe("I");
+    });
+
+    it("should return II for 2", () => {
+        expect(roman.convert(2)).toBe("II");
+    });
+    it("should return III for 3", () => {
+        expect(roman.convert(3)).toBe("III");
+    })
+})
