b97e2eaffee2d0f11a6d177c80842087feb2bd92
Javier Chacana <javier.chacana@codurance.com>
2020-10-25 17:42:17 +0100

    Refactor: rename
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman-tpp/roman.spec.ts
+++ src/kata/roman-tpp/roman.spec.ts
@@ -2,8 +2,8 @@ import {RomanTPPConverter} from "./roman";
 
 describe("Roman TPP converter", () => {
     it("should return I when converting 1", () => {
-        const romanConverter = new RomanTPPConverter();
-        expect(romanConverter.convert(1)).toBe("I");
+        const romanTPPConverter = new RomanTPPConverter();
+        expect(romanTPPConverter.convert(1)).toBe("I");
     });
     it("should return II when converting 2", () => {
         const romanTPPConverter = new RomanTPPConverter();
