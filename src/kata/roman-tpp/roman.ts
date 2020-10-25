f4142a7c5d89561419048c9a2ab421d70571fa65
Javier Chacana <javier.chacana@codurance.com>
2020-10-25 17:44:44 +0100

    Green: unconditional to conditional transformation. Now we have duplication
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman-tpp/roman.ts
+++ src/kata/roman-tpp/roman.ts
@@ -2,7 +2,9 @@ export class RomanTPPConverter {
     convert(decimal: number) {
         let roman = "I";
         if(decimal > 1)
-            roman += "I"
+            roman += "I";
+        if(decimal > 2)
+            roman += "I";
         return roman;
     }
 }
