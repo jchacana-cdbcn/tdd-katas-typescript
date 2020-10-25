85e384245c28dfc9adf4292a10a58941a2f00c7f
Javier Chacana <javier.chacana@codurance.com>
2020-10-25 18:26:03 +0100

    Green: Added new transformation (tail recursion again) to remove duplication
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman-tpp/roman.ts
+++ src/kata/roman-tpp/roman.ts
@@ -1,16 +1,16 @@
 const romanDictionary: Map<number, string> = new Map<number, string>([
     [1, "I"],
     [4, "IV"],
-    [5, "V"],
-    [6, "VI"],
-    [7, "VII"],
-    [8, "VIII"]
+    [5, "V"]
 ])
 
 export class RomanTPPConverter {
     convert(decimal: number): string {
         if(romanDictionary.has(decimal))
             return <string>romanDictionary.get(decimal);
+        if(decimal > 5 ) {
+            return romanDictionary.get(5) + this.convert(decimal - 5);
+        }
         return romanDictionary.get(1) + this.convert(decimal - 1)
     }
 }
