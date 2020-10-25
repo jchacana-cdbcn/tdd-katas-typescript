2dc68f6b5ee489934d34065ecbef87235359ff2e
Javier Chacana <javier.chacana@codurance.com>
2020-10-25 17:31:37 +0100

    RED: Failing test for case 2
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman-tpp/roman.spec.ts
+++ src/kata/roman-tpp/roman.spec.ts
@@ -3,6 +3,10 @@ import {RomanTPPConverter} from "./roman";
 describe("Roman TPP converter", () => {
     it("should return I when converting 1", () => {
         const romanConverter = new RomanTPPConverter();
-        expect(romanConverter.convert(1)).toBe("I")
-    })
+        expect(romanConverter.convert(1)).toBe("I");
+    });
+    it("should return II when converting 2", () => {
+        const romanTPPConverter = new RomanTPPConverter();
+        expect(romanTPPConverter.convert(2)).toBe("II");
+    });
 })
