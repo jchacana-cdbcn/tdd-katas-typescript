73879a23ee55cb63c9e6cda71d00211f32344197
Javier Chacana <javier.chacana@codurance.com>
2020-10-25 18:02:09 +0100

    Green: Applying next transformation Array -> Container. Still duplication on character "I"
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman-tpp/roman.ts
+++ src/kata/roman-tpp/roman.ts
@@ -1,6 +1,12 @@
+const romanDictionary: Map<number, string> = new Map<number, string>([
+    [1, "I"],
+    [2, "II"],
+    [3, "III"],
+    [4, "IV"]
+])
+
 export class RomanTPPConverter {
     convert(decimal: number) {
-        let roman = ["I", "II", "III", "IV"];
-        return roman[decimal-1];
+        return romanDictionary.get(decimal);
     }
 }
