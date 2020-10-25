e7772affba127bf5bb59f607ed9f55ff10fb4a1f
Javier Chacana <javier.chacana@codurance.com>
2020-10-25 21:59:52 +0100

    Green: Applied again transformation of loop to loop. Not convinced
    Signed-off-by: Javier Chacana <javier.chacana@codurance.com>

--- src/kata/roman-tpp/roman.ts
+++ src/kata/roman-tpp/roman.ts
@@ -1,26 +1,23 @@
 const romanDictionary: Map<number, string> = new Map<number, string>([
-    [1, "I"],
-    [4, "IV"],
-    [5, "V"],
-    [9, "IX"],
-    [10, "X"],
     [40, "XL"],
-
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"]
 ])
 
 export class RomanTPPConverter {
     convert(decimal: number): string {
         if(romanDictionary.has(decimal))
             return <string>romanDictionary.get(decimal);
-        if(decimal > 40) {
-            return romanDictionary.get(40) + this.convert(decimal - 40);
-        }
-        if(decimal > 10) {
-            return romanDictionary.get(10) + this.convert(decimal - 10);
-        }
-        if(decimal > 5 ) {
-            return romanDictionary.get(5) + this.convert(decimal - 5);
-        }
-        return romanDictionary.get(1) + this.convert(decimal - 1)
+        let roman = "";
+        romanDictionary.forEach(((value, key) =>{
+            while (decimal >= key) {
+                roman += value;
+                decimal -= key;
+            }
+        }))
+        return roman;
     }
 }
