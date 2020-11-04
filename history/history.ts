a935e99300edb30c7e764f201bbf44e89d80c460
Lau Llobet <lau.llobet@mypulse.ai>
2020-11-04 11:12:01 +0100

    --wip-- [skip ci]

--- history/history.ts
+++ history/history.ts
@@ -19,7 +19,12 @@ export class NotThisPlayerTurnError extends Error {
 }
 
 export class Game {
+    private currentPlayerIs1: boolean = true
+
     user1PlaysAt(arg0: number, arg1: number) {
-       
+        if(! this.currentPlayerIs1){
+            throw new NotThisPlayerTurnError()
+        }
+        this.currentPlayerIs1 = false
     }
 }
