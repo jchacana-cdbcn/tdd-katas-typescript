5246869c36b262ef67cdcf31535ea23ca30d5fc4
Lau Llobet <lau.llobet@mypulse.ai>
2020-11-04 11:25:43 +0100

    --wip-- [skip ci]

--- history/history.ts
+++ history/history.ts
@@ -11,15 +11,15 @@ describe('Tic Tac Toe', function () {
 
         var game2: Game = new Game()
         game2.user1PlaysAt(1,1)
-        expect(()=>{game2.user2PlaysAt(0,0)}).not.toThrow()
-        expect(() => {game2.user2PlaysAt(2,2)}).toThrowError()
+        expect(()=> game2.user2PlaysAt(0,0)).not.toThrow()
+        expect(() => game2.user2PlaysAt(2,2) ).toThrowError()
 
         //transitive
         var game3: Game = new Game()
         game3.user1PlaysAt(1,1)
-        expect(()=>{game3.user2PlaysAt(0,0)}).not.toThrow()
-        expect(()=>{game3.user1PlaysAt(2,2)}).not.toThrow()
-        expect(()=>{game3.user2PlaysAt(1,0)}).not.toThrow()
+        expect(()=> game3.user2PlaysAt(0,0) ).not.toThrow()
+        expect(()=> game3.user1PlaysAt(2,2) ).not.toThrow()
+        expect(()=> game3.user2PlaysAt(1,0) ).not.toThrow()
     })
 
 
