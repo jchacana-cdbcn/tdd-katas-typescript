2fb05a45d2c0159cfddee6292a11a8f26336b592
Lau Llobet <lau.llobet@mypulse.ai>
2020-11-04 11:20:10 +0100

    --wip-- [skip ci]

--- src/kata/tictactoe-pairing/tictactoe.spec.ts
+++ src/kata/tictactoe-pairing/tictactoe.spec.ts
@@ -13,6 +13,13 @@ describe('Tic Tac Toe', function () {
         game2.user1PlaysAt(1,1)
         expect(()=>{game2.user2PlaysAt(0,0)}).not.toThrow()
         expect(() => {game2.user2PlaysAt(2,2)}).toThrowError()
+
+        var game3: Game = new Game()
+        game3.user1PlaysAt(1,1)
+        expect(()=>{game3.user2PlaysAt(0,0)}).not.toThrow()
+        expect(()=>{game3.user1PlaysAt(2,2)}).not.toThrow()
+        expect(()=>{game3.user2PlaysAt(1,0)}).not.toThrow()
+    
     })
 
 });
