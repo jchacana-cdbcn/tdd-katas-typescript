113d72e74ffbf4e1f44aba593db90290cdab8fb2
Lau Llobet <lau.llobet@mypulse.ai>
2020-11-04 11:16:16 +0100

    RED wip - [skip ci]

--- src/kata/tictactoe-pairing/tictactoe.spec.ts
+++ src/kata/tictactoe-pairing/tictactoe.spec.ts
@@ -6,9 +6,13 @@ describe('Tic Tac Toe', function () {
     // start from corner cases
     it('should not allow same user playing twice', function() {
         var game: Game = new Game()
-
         game.user1PlaysAt(1,1)
-
         expect(() => {game.user1PlaysAt(0,0)}).toThrowError(NotThisPlayerTurnError)
+
+        var game2: Game = new Game()
+        game2.user1PlaysAt(1,1)
+        expect(()=>{game2.user2PlaysAt(0,0)}).not.toThrow()
+       // expect(() => {game2.user2PlaysAt(2,2)}).toThrowError()
     })
+
 });
