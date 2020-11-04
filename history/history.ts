d525c16a332623fa202bd136c79fefaffd5f307c
Lau Llobet <lau.llobet@mypulse.ai>
2020-11-04 11:09:37 +0100

    RED wip - [skip ci]

--- history/history.ts
+++ history/history.ts
@@ -1,7 +1,25 @@
+import {Game, NotThisPlayerTurnError} from "./Game";
+
+
 describe('Tic Tac Toe', function () {
-    it('something', function() {
-        
+
+    // start from corner cases
+    it('should not allow same user playing twice', function() {
+        var game: Game = new Game()
+
+        game.user1PlaysAt(1,1)
+
+        expect(() => {game.user1PlaysAt(0,0)}).toThrowError(NotThisPlayerTurnError)
     })
 });
 //-------- IMPLEMENTATION --------
 
+export class NotThisPlayerTurnError extends Error {
+
+}
+
+export class Game {
+    user1PlaysAt(arg0: number, arg1: number) {
+       
+    }
+}
---
+++ src/kata/tictactoe-pairing/Game.ts
@@ -0,0 +1,9 @@
+export class NotThisPlayerTurnError extends Error {
+
+}
+
+export class Game {
+    user1PlaysAt(arg0: number, arg1: number) {
+       
+    }
+}
