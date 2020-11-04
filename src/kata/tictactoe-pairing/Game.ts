caa435e002b1a0619aa9101fa8b7e65db9432360
Lau Llobet <lau.llobet@mypulse.ai>
2020-11-04 11:23:20 +0100

    --wip-- [skip ci]

--- src/kata/tictactoe-pairing/Game.ts
+++ src/kata/tictactoe-pairing/Game.ts
@@ -3,18 +3,19 @@ export class NotThisPlayerTurnError extends Error {
 }
 
 export class Game {
+    private currentPlayerIsX: boolean = true
+    
     user2PlaysAt(arg0: number, arg1: number) {
-        if(this.currentPlayerIs1){
+        if(this.currentPlayerIsX){
             throw new NotThisPlayerTurnError()
         }
-        this.currentPlayerIs1 = true
+        this.currentPlayerIsX = true
     }
-    private currentPlayerIs1: boolean = true
 
     user1PlaysAt(arg0: number, arg1: number) {
-        if(! this.currentPlayerIs1){
+        if(! this.currentPlayerIsX){
             throw new NotThisPlayerTurnError()
         }
-        this.currentPlayerIs1 = false
+        this.currentPlayerIsX = false
     }
 }
